69a3bb86dd69ea7914970477ed3c5f9f96bad732
Mr.Smoke <kumroshan120@gmail.com>
2023-06-22 20:11:53 +0530

    update

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -1,19 +1,27 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useState } from 'react';
+import axios from 'axios';
+
 import '../assets/CSS/Header.css';
 
 import { UserCreateContext } from '../Pages/UserCreateContext';
+import { Navigate } from 'react-router-dom';
 
 export const Header = () => {
-  const { user } = useContext(UserCreateContext);
+  const { ready, user , setUser} = useContext(UserCreateContext);
+  const [redirectHomePage, setRedirectHomePage] = useState(null);
 
-  const signOutHandler = (ev) => {
-    ev.preventDefault();
-    fetch('http://localhost:4000/signOut', {
-      method: 'POST',
-      credentials: 'include',
-    });
-    setUserInfo(null);
+  const logoutHandler = async () => {
+    await axios.post('/logout');
+    setRedirectHomePage('/');
+    setUser(null);
   };
+  if (redirectHomePage) {
+    return <Navigate to={redirectHomePage} />;
+  }
+
+  if (ready && !user && !redirectHomePage) {
+    return <Navigate to="/login" />;
+  }
 
   return (
     <div className="main_container">
@@ -23,7 +31,7 @@ export const Header = () => {
             <div className="header_logo">Welcome, {user.name}</div>
             <div className="nav-items">
               <button className="signOut_btn">
-                <a onClick={signOutHandler}>Sign out</a>
+                <a onClick={logoutHandler}>Sign out</a>
               </button>
             </div>
           </>
